82fcac9992e676b9d35bfdfca6178a60595f93b2
USUARIO <USUARIO@PC17>
2019-10-10 10:29:42 -0500

    Se instancian las clases

---
+++ src/parcial/Parcial.java
@@ -0,0 +1,36 @@
+package parcial;
+
+public class Parcial {
+     
+    private int id;
+    private String fecha;
+    private String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+       
+       
+    }
+    
+
